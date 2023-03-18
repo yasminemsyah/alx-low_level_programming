08701956f5ac1986f71743cb800f962cadb21ad3
Yasmine Msyah <msyah.yasmine@gmail.com>
2023-03-18 14:49:28 -0700

    Trying my best

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
- #include <stdio.h>
-   /**
-      *main - prints a string using printf
-      *Description: My C learning parkour using betty style coding
-      *Return: value should be always Inside ()
-   */
-  int main(void)
-  {
-	printf("with proper grammar, but the outcome is a piece of art,");
-  	return (0);
-  }   
+#include <stdio.h>
+
+/**
+  *main - prints a string using printf
+  *Description: My C Learning Parkour Using Betty coding style
+  *Return: should be always inside ()
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
