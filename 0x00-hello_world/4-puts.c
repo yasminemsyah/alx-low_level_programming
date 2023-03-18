7661623b1871e9cde1caaa72d82a5fe5e76f5707
Yasmine Msyah <msyah.yasmine@gmail.com>
2023-03-18 13:40:22 -0700

    Trying my best

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
    *main : prints a string using puts
    *Return: value should be always Inside ()
 */
-char main(void)
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
 	return (0);
