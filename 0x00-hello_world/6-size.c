f6af08a3a96188d3bf26c5e8bcd647686e0e6c88
Yasmine Msyah <msyah.yasmine@gmail.com>
2023-03-18 18:01:11 -0700

    Trying My Best

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  *main - Print Sizes Of Various Types Of Data 
+  *Description: My C Learning Parkour Using Betty Coding Style
+  *Return:Should be Always Inside ()
+*/
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)");
+}
