5563a8d950d4a8563f41857feef42524821b15f8
Yasmine Msyah <msyah.yasmine@gmail.com>
2023-03-19 07:34:46 -0700

    Trying My Best

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  *main - Print a String Using Putchar
+  *Description:My C Learning parkour using betty Coding Style
+  *return: Should be always inside ()
+*/
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
